bd7e66036a4eb872a4eb9146b1d81e7653a2e6f0
TomaszSteblik <tsteblik@gmail.com>
2022-06-22 19:33:57 +0200

    cleanup

---
+++ ConsoleRunner/DataReaderCsvHelper.cs
@@ -0,0 +1,53 @@
+using System.Data;
+using System.Globalization;
+using CsvHelper;
+using GeneticAlgorithm.Models;
+
+namespace ConsoleRunner;
+
+public static class DataReaderHelper
+{
+    internal static Person[] GetPeopleFromCsv(string? parametersDataPathPersonel)
+    {
+        var people = new List<Person>();
+        if (parametersDataPathPersonel is null)
+            throw new ArgumentNullException(nameof(parametersDataPathPersonel),"Null path to personel");
+        using (var reader = new StreamReader(parametersDataPathPersonel))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            var records = csv.GetRecords<PersonelHelper>();
+            var enumerable = records.ToList();
+            for (var index = 0; index < enumerable.Count; index++)
+            {
+                var record = enumerable[index];
+                if (record.Qualifications is null)
+                    throw new DataException(nameof(record));
+                var person = new Person
+                {
+                    Id = index,
+                    Name = record.Name,
+                    Qualifications = new List<Qualification>()
+                };
+
+                var qualifications = record.Qualifications.Split('-');
+                foreach (var qualification in qualifications)
+                {
+                    person.Qualifications.Add(Enum.Parse<Qualification>(qualification));
+                }
+                people.Add(person);
+            }
+        }
+
+        return people.ToArray();
+    }
+
+    internal static Machine[] GetMachinesFromCsv(string? parametersDataPathMachines)
+    {
+        if (parametersDataPathMachines is null)
+            throw new ArgumentNullException(nameof(parametersDataPathMachines), "Null path to machines");
+        using var reader = new StreamReader(parametersDataPathMachines);
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+        var records = csv.GetRecords<Machine>();
+        return records.ToArray();
+    }
+}
