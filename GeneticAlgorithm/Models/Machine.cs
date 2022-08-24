d33bdc947f9f655d5101fc19d3bd9b7376fde76b
TomaszSteblik <tsteblik@gmail.com>
2022-08-24 18:55:03 +0200

    removed personel count

--- GeneticAlgorithm/Models/Machine.cs
+++ GeneticAlgorithm/Models/Machine.cs
@@ -6,8 +6,6 @@ public class Machine
 {
     [Name("qualification")]
     public Qualification RequiredQualification{ get; set; }
-    [Name("personelCount")]
-    public int PersonelCount { get; set; }
     [Name("name")]
     public string? Name { get; set; }
 }
