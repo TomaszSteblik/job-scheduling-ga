5c71ffacbd97e080c676129d7743069560ee2fd7
TomaszSteblik <tsteblik@gmail.com>
2022-08-30 15:28:26 +0200

    result generation method extraction

--- Scheduling/MapperProfiles/AlgorithmResultProfile.cs
+++ Scheduling/MapperProfiles/AlgorithmResultProfile.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using AutoMapper;
@@ -19,79 +20,104 @@ public class AlgorithmResultProfile : Profile
 
     private static IEnumerable<object> GenerateScheduleFromChromosome(Chromosome chromosome, Machine[] machines)
     {
+        var propNames = machines.Select((x, i) => x.Name ?? $"Unknown_{i}").ToArray();
+            
+        var asmBuilder = GenerateTemporaryAssembly();
+        var type = GenerateTemporaryType(propNames, asmBuilder);
+        
+        var scheduledDaysList = GenerateTypeList(chromosome.Value.Select(x => x.Select(z => z.Id.ToString()).ToArray()), 
+            propNames, asmBuilder, type);
+        
+        return scheduledDaysList;
+    }
+
+    private static AssemblyBuilder GenerateTemporaryAssembly()
+    {
         var assemblyName = new AssemblyName
-            {
-                Name = "DynamicObject"
-            };
-            var asmBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+        {
+            Name = "DynamicObject"
+        };
+        var asmBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+        return asmBuilder;
+    }
+
+    private static Type GenerateTemporaryType(IEnumerable<string> names, AssemblyBuilder asmBuilder)
+    {
+        var modBuilder = asmBuilder.DefineDynamicModule("DynamicModule");
+        var typeBuilder = modBuilder.DefineType("ScheduledDay", TypeAttributes.Public);
+
+        foreach (var name in names)
+        {
+            var propertyBuilder = typeBuilder.DefineProperty(name,
+                PropertyAttributes.None,
+                CallingConventions.HasThis,
+                typeof(string), Type.EmptyTypes);
             
-            var modBuilder = asmBuilder.DefineDynamicModule("DynamicModule");
-            
-            var typeBuilder = modBuilder.DefineType("ScheduledDay",TypeAttributes.Public);
+            var backingFieldBuilder = typeBuilder.DefineField($"_machine.Name",
+                typeof(string),
+                FieldAttributes.Private);
 
-            for (var machineNumber = 0; machineNumber < machines.Length; machineNumber++)
+            var getter = GenerateGetter(typeBuilder, name, backingFieldBuilder);
+            propertyBuilder.SetGetMethod(getter);
+
+            var setter = GenerateSetter(typeBuilder, name, backingFieldBuilder);
+            propertyBuilder.SetSetMethod(setter);
+        }
+
+        var type = typeBuilder.CreateType() ?? throw new Exception("Creating temporary type failed during mapping");
+        return type;
+    }
+
+    private static MethodBuilder GenerateSetter(TypeBuilder typeBuilder, string propName, FieldBuilder backingFieldBuilder)
+    {
+        var setterBuilder =
+            typeBuilder.DefineMethod($"{propName}_getter",
+                MethodAttributes.Public,
+                null,
+                new[] {typeof(string)});
+
+        var setterIl = setterBuilder.GetILGenerator();
+
+        setterIl.Emit(OpCodes.Ldarg_0);
+        setterIl.Emit(OpCodes.Ldarg_1);
+        setterIl.Emit(OpCodes.Stfld, backingFieldBuilder);
+        setterIl.Emit(OpCodes.Ret);
+        
+        return setterBuilder;
+    }
+
+    private static MethodBuilder GenerateGetter(TypeBuilder typeBuilder, string propName, FieldBuilder backingFieldBuilder)
+    {
+        var getterBuilder = typeBuilder.DefineMethod($"{propName}_getter",
+            MethodAttributes.Public, typeof(string), Type.EmptyTypes);
+
+        var getterIl = getterBuilder.GetILGenerator();
+        
+        getterIl.Emit(OpCodes.Ldarg_0);
+        getterIl.Emit(OpCodes.Ldfld, backingFieldBuilder);
+        getterIl.Emit(OpCodes.Ret);
+        
+        return getterBuilder;
+    }
+
+    private static IEnumerable<object> GenerateTypeList(IEnumerable<string[]> values, string[] propNames, AssemblyBuilder asmBuilder, Type type)
+    {
+        var scheduledDaysList = new List<object>();
+        foreach (var value in values)
+        {
+            var obj = asmBuilder.CreateInstance(type.FullName ??
+                                                throw new Exception("Missing namespace during creation "))
+                      ?? throw new Exception("Failed to create the temporary the object");
+            for (var i = 0; i < propNames.Length; i++)
             {
-                var machine = machines[machineNumber];
-                var propertyBuilder = typeBuilder.DefineProperty(machine.Name ?? $"Unknown_{machineNumber}", PropertyAttributes.None,
-                    CallingConventions.HasThis,
-                    typeof(string), Type.EmptyTypes);
-
-                //define backing field
-
-                var backingFieldBuilder = typeBuilder.DefineField($"_machine.Name",
-                    typeof(string),
-                    FieldAttributes.Private);
-
-
-                //define getter
-                var getterBuilder = typeBuilder.DefineMethod($"{machine.Name}_getter",
-                    MethodAttributes.Public, typeof(string), Type.EmptyTypes);
-
-
-                var getterIl = getterBuilder.GetILGenerator();
-                getterIl.Emit(OpCodes.Ldarg_0);
-                getterIl.Emit(OpCodes.Ldfld, backingFieldBuilder);
-                getterIl.Emit(OpCodes.Ret);
-
-                propertyBuilder.SetGetMethod(getterBuilder);
-
-                // Define the "set" accessor method for CustomerName.
-                var setterBuilder =
-                    typeBuilder.DefineMethod($"{machine.Name}_getter",
-                        MethodAttributes.Public,
-                        null,
-                        new[] {typeof(string)});
-
-                var setterIl = setterBuilder.GetILGenerator();
-
-                setterIl.Emit(OpCodes.Ldarg_0);
-                setterIl.Emit(OpCodes.Ldarg_1);
-                setterIl.Emit(OpCodes.Stfld, backingFieldBuilder);
-                setterIl.Emit(OpCodes.Ret);
-
-                propertyBuilder.SetSetMethod(setterBuilder);
+                var name = propNames[i];
+                var propInfo = type.GetProperty(name);
+                propInfo?.SetValue(obj, value[i]);
             }
 
-            var type = typeBuilder.CreateType() ?? throw new Exception("Creating temporary type failed during mapping");
+            scheduledDaysList.Add(obj);
+        }
 
-            
-            var scheduledDaysList = new List<object>();
-            foreach (var day in chromosome.Value)
-            {
-                var obj = asmBuilder.CreateInstance(type.FullName ?? 
-                                                    throw new Exception("Missing namespace during creation ")) 
-                          ?? throw new Exception("Failed to create the temporary the object");
-                for (var i = 0; i < machines.Length; i++)
-                {
-                    var propInfo = type.GetProperty(machines[i].Name ?? $"Unknown_{i}");
-                    propInfo?.SetValue(obj, day[i].Id.ToString());
-                }
-                
-
-               
-                scheduledDaysList.Add(obj);
-            }
-
-            return scheduledDaysList;
+        return scheduledDaysList;
     }
 }
